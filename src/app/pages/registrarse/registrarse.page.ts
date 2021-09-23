1955503c0603e08567ab25cfdadbe40f2b2b99a9
PanconChancho <rodricontre1234@gmail.com>
2021-09-22 22:41:05 -0300

    Se arreglo pagina Regristrarse

--- src/app/pages/registrarse/registrarse.page.ts
+++ src/app/pages/registrarse/registrarse.page.ts
@@ -1,11 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormGroup,
-  FormControl,
-  Validators,
-  FormBuilder
-} from '@angular/forms';
-import { AlertController } from '@ionic/angular';
 
 
 @Component({
@@ -14,42 +7,22 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./registrarse.page.scss'],
 })
 export class RegistrarsePage implements OnInit {
-  
-  formularioRegistrarse: FormGroup;
 
-  constructor(public fb: FormBuilder, 
-    public alertController: AlertController) { 
+  constructor(){}
 
-    this.formularioRegistrarse = this.fb.group({
-      'nombre': new FormControl("",Validators.required),
-      'email': new FormControl("",Validators.required),
-      'numero': new FormControl("",Validators.required),
-      'password': new FormControl("",Validators.required),
-      'ConfirmacionPassword': new FormControl("",Validators.required)
-    });
-
+  usuario = {
+    email:'',
+    password:''
   }
+
 
   ngOnInit() {
   }
-  async guardar(){
-    var f = this .formularioRegistrarse.value;
 
-    if(this.formularioRegistrarse.invalid){
-      const alert = await this.alertController.create({
-        header:'Datos incompletos',
-        message: 'Tienes que llenar todos los datos',
-        buttons: ['Aceptar']
-      });
-
-      await alert.present();
-      return;
-    }
-
-    var usuario = {
-      nombre: f.nombre,
-      password: f.password
-    }
-    localStorage.setItem('usuario',JSON.stringify(usuario))
+  onSubmit(){
+    console.log('submit');
+    console.log(this.usuario);
   }
+  
+
 }
